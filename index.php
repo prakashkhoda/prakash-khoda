b81d38598a286f80373049bae85136041dc11fff
prakashkhoda <khodaprakash099@gmail.com>
2026-05-30 08:38:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Food order website - Home page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+    <div class="Menu text-center">
+        <div class="wrapper">
+        <ul>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Admin</a></li>
+           <li><a href="#">Category</a></li>
+           <li><a href="#">Food</a></li>
+           <li><a href="#">Order</a></li>
+
+        </ul>
+    </div>
+
+    </div>
+    <div class="Main-content">
+    <div class="wrapper">
+       <strong>DASHBORD</strong>
+
+       <div class="col-4 text-center">
+        <h1>5</h1>
+        categories
+       </div>
+    </div>
+    </div>
+
+    <div class="footer">
+        <div class="wrapper">
+           <p class="text-center"> 2026 All rights reserved,some Restaurant,Developed by - <a href="#">Prakash khoda</a></p>
+        </div>
+    </div>
+
+    </body>
+</html>
